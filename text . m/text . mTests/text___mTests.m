a77d3173e14583c2690c28a4bb48cc467bed5163
shirley <zkyboy@126.com>
2015-12-29 15:30:41 +0800

    test Git

---
+++ text . m/text . mTests/text___mTests.m
@@ -0,0 +1,39 @@
+//
+//  text___mTests.m
+//  text . mTests
+//
+//  Created by tarena on 15/12/29.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface text___mTests : XCTestCase
+
+@end
+
+@implementation text___mTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
